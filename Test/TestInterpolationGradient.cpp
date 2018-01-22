636c3acf243dccd3ee1cf810ff54d6d8b54f8574
Tanner Schmidt <tws10@cs.washington.edu>
2018-01-22 14:21:25 -0800

    TestInterpolationGradient for integral index

--- Test/TestInterpolationGradient.cpp
+++ Test/TestInterpolationGradient.cpp
@@ -25,15 +25,22 @@ TEST(InterpolationGradientTest, Test2DVectorValued) {
 
     ASSERT_TRUE(gradientTypeMatch);
 
-    Eigen::Vector2f data[] = { {0.0f,  0.0f}, {1.0f, -1.0f}, {2.0f, -2.0f}, { 3.0f, -3.0f},
-                               {3.0f, -3.0f}, {5.0f, -5.0f}, {4.0f, -4.0f}, { 0.0f,  0.0f},
-                               {6.0f, -6.0f}, {7.0f, -7.0f},{-2.0f,  2.0f}, {-1.0f,  1.0f} };
+    Eigen::Vector2f data[] = { { 0.0f,  0.0f}, { 1.0f, -1.0f}, { 2.0f, -2.0f}, { 3.0f, -3.0f},
+                               { 3.0f, -3.0f}, { 5.0f, -5.0f}, { 4.0f, -4.0f}, { 0.0f,  0.0f},
+                               { 6.0f, -6.0f}, { 7.0f, -7.0f}, {-2.0f,  2.0f}, {-1.0f,  1.0f} };
 
     Image<Eigen::Vector2f> image( { 4, 3}, data);
 
     std::cout << std::endl;
 
-    Eigen::Matrix<float, 2, 2> g = image.InterpolationGradient(0.5f, 0.5f);
+    Eigen::Matrix<float, 2, 2> g = image.InterpolationGradient(0.5f, 0.5);
+
+    ASSERT_NEAR(1.5f, g(0,0), 1e-4);  ASSERT_NEAR(-1.5f, g(0,1), 1e-4);
+    ASSERT_NEAR(3.5f, g(1,0), 1e-4);  ASSERT_NEAR(-3.5f, g(1,1), 1e-4);
+
+    std::cout << g << std::endl;
+
+    g = image.InterpolationGradient(2, 1.5f);
 
     std::cout << g << std::endl;
 
